15247cf3575f5ddfd34e543fb701f2af89a224d3
Lado Samushia <lado.samushia.office@gmail.com>
2025-03-05 10:59:59 -0600

    modified generate galaxies to avoid the error with casting

--- src/populate_galaxies.py
+++ src/populate_galaxies.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy as sp
 
-def populate_galaxies(data_dict, HODparams):
+def generate_galaxies(data_dict, HODparams):
     """
     Generate galaxies based on halo and subsample data using Halo Occupation Distribution (HOD) parameters.
     
@@ -19,21 +19,21 @@ def populate_galaxies(data_dict, HODparams):
     tuple of numpy arrays
         (x_galaxies, y_galaxies, z_galaxies)
     """
-    # Unpack halo data
-    Mh = data_dict['halo']['mass']
-    xh = data_dict['halo']['x']
-    yh = data_dict['halo']['y']
-    zh = data_dict['halo']['z']
-    sh = data_dict['halo']['sigma']
+    # Unpack halo data and ensure float64 type
+    Mh = np.asarray(data_dict['halo']['mass'], dtype=np.float64)
+    xh = np.asarray(data_dict['halo']['x'], dtype=np.float64)
+    yh = np.asarray(data_dict['halo']['y'], dtype=np.float64)
+    zh = np.asarray(data_dict['halo']['z'], dtype=np.float64)
+    sh = np.asarray(data_dict['halo']['sigma'], dtype=np.float64)
     
-    # Unpack subsample data
-    Ms = data_dict['subsample']['mass']
-    vhost = data_dict['subsample']['host_velocity']
-    ns = data_dict['subsample']['n_particles']
-    xs = data_dict['subsample']['x']
-    ys = data_dict['subsample']['y']
-    zs = data_dict['subsample']['z']
-    vs = data_dict['subsample']['velocity']
+    # Unpack subsample data and ensure float64 type
+    Ms = np.asarray(data_dict['subsample']['mass'], dtype=np.float64)
+    vhost = np.asarray(data_dict['subsample']['host_velocity'], dtype=np.float64)
+    ns = np.asarray(data_dict['subsample']['n_particles'], dtype=np.float64)
+    xs = np.asarray(data_dict['subsample']['x'], dtype=np.float64)
+    ys = np.asarray(data_dict['subsample']['y'], dtype=np.float64)
+    zs = np.asarray(data_dict['subsample']['z'], dtype=np.float64)
+    vs = np.asarray(data_dict['subsample']['velocity'], dtype=np.float64)
     
     # Unpack HOD parameters
     lnMcut, sigma, lnM1, kappa, alpha, alpha_c, alpha_s = HODparams
