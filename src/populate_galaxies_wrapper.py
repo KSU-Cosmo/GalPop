46c5dd5e111f5e647d205cd2974df0a22f4b6ac1
ladosamushia <colkhis@gmail.com>
2025-03-10 22:01:01 -0500

    linted pg wrapper

--- src/populate_galaxies_wrapper.py
+++ src/populate_galaxies_wrapper.py
@@ -1,16 +1,16 @@
 import numpy as np
-import scipy as sp
 import os
-
 import julia
+
 julia.Julia(compiled_modules=False)  # Initializes Julia
 
-from julia import Main  # Imports Julia's Main module
+from julia import Main  # noqa: E402
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 julia_file = os.path.join(current_dir, "populate_galaxies.jl")
 Main.include(julia_file)
+
 
 # Python wrapper for the Julia function that accepts individual arrays
 def populate_galaxies(
@@ -20,7 +20,7 @@ def populate_galaxies(
 ):
     """
     Python wrapper for the Julia implementation of populate_galaxies that accepts individual arrays.
-    
+
     Parameters
     ----------
     h_mass : array-like
@@ -49,17 +49,17 @@ def populate_galaxies(
         Minimum box coordinate, by default -1000
     Lmax : float, optional
         Maximum box coordinate, by default 1000
-        
+
     Returns
     -------
     dict
         Dictionary of galaxy positions with keys 'x', 'y', 'z'
     """
-    
+
     print("unpacking HOD parameters")
     # Unpack HOD parameters
     lnMcut, sigma, lnM1, kappa, alpha, alpha_c, alpha_s = HODparams
-    
+
     print("call the Julia function")
     # Call the Julia function
     x_gal, y_gal, z_gal = Main.populate_galaxies_julia(
@@ -68,13 +68,13 @@ def populate_galaxies(
         lnMcut, sigma, lnM1, kappa, alpha, alpha_c, alpha_s,
         rsd, float(Lmin), float(Lmax)
     )
-    
+
     print("convert the arrays back to python")
     # Convert Julia arrays to NumPy arrays
     x_gal = np.array(x_gal)
     y_gal = np.array(y_gal)
     z_gal = np.array(z_gal)
-    
+
     # Return dictionary of galaxy positions
     return {
         'x': x_gal,
