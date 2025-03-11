3e6734bd41ce34dc895ae36aa62dfdbb941c4edf
ladosamushia <colkhis@gmail.com>
2025-03-10 21:58:46 -0500

    linted test pg wrapper

--- tests/test_populate_galaxies_wrapper.py
+++ tests/test_populate_galaxies_wrapper.py
@@ -1,18 +1,18 @@
 import numpy as np
-import pytest
-import sys
 import os
-from unittest.mock import mock_open, patch
+import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+srcpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
+sys.path.append(srcpath)
 
-import populate_galaxies_wrapper as pgw
+import populate_galaxies_wrapper as pgw # noqa: E402
+
 
 return_xyz = {'x': [1, 2],
               'y': [3, 4],
               'z': [5, 6]}
 
-#@patch ('Main.populate_galaxies_julia', return_value=return_xyz)
+
 def test_populate_galaxies():
     # Create mock halo and subsample dictionaries with test data
     h_mass = np.array([1e13, 2e13, 3e13]),  # Halo masses
@@ -21,7 +21,7 @@ def test_populate_galaxies():
     h_z = np.array([0, 10, 20]),  # z coordinates
     h_sigma = np.array([0.2, 0.3, 0.4]),  # sigma values
     h_velocity = np.array([10, 20, 30])  # Velocities
-    
+
     s_mass = np.array([1e14, 2e14, 3e14]),  # Subsample masses
     s_host_velocity = np.array([1, 2, 3]),  # Host velocities
     s_n_particles = np.array([100, 200, 300]),  # Number of particles
@@ -29,7 +29,7 @@ def test_populate_galaxies():
     s_y = np.array([100, 110, 120]),  # y coordinates
     s_z = np.array([100, 110, 120]),  # z coordinates
     s_velocity = np.array([10, 20, 30])  # Velocities
-    
+
     # Mock HOD parameters 
     # [lnMcut, sigma, lnM1, kappa, alpha, alpha_c, alpha_s]
     hod_params = [
@@ -41,10 +41,12 @@ def test_populate_galaxies():
         0.1,             # alpha_c
         0.5              # alpha_s
     ]
-    
+
     # Call the populate_galaxies function with separate h and s inputs
-    galaxies = pgw.populate_galaxies(h_mass, h_x, h_y, h_z, h_velocity, h_sigma, s_mass, s_host_velocity, 
-        s_n_particles, s_x, s_y, s_z, s_velocity, hod_params)
+    galaxies = pgw.populate_galaxies(
+        h_mass, h_x, h_y, h_z, h_velocity, h_sigma, s_mass, s_host_velocity, 
+        s_n_particles, s_x, s_y, s_z, s_velocity, hod_params
+        )
 
     # 1. Check that the output arrays have the same length
     assert galaxies['x'] == [1, 2]
