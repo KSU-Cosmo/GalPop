19dd83f439b56e286fa837e3945afc41bcb6a190
ladosamushia <colkhis@gmail.com>
2025-03-11 22:48:36 -0500

    added a test

--- julia/test/hod_tests.jl
+++ julia/test/hod_tests.jl
@@ -4,6 +4,7 @@ using Test
 using Random
 using Statistics
 using GalPop
+using Mocking
 
 @testset "HOD Module Tests" begin
     
@@ -41,4 +42,44 @@ using GalPop
         n_sat = GalPop.calculate_n_sat(Mh, Mcut, M1, alpha, kappa, 1.0)
         @test isapprox(n_sat, 100, atol = 1e-4)
     end
+
+    @testset "populate_galaxies" begin
+
+        halos = (
+            mass = [10^11, 10^12, 10^14, 10^15],
+            x = [1.0, 2.0, 3.0, 4.0],
+            y = [1.0, 2.0, 3.0, 4.0],
+            z = [999.0, 999.0, 0.0, -999.0],
+            velocity = [2.0, -1.0, 0.0, -2.0],
+            sigma = [1.0, 1.0, 1.0, 1.0]
+            )
+        
+        subhalos = (
+            mass = [10^11, 10^12, 10^14, 10^15],
+            host_velocity = [1.0, 2.0, 3.0, 4.0],
+            n_particles = [10, 10, 10, 10],
+            x = [1.0, 2.0, 3.0, 4.0],
+            y = [1.0, 2.0, 3.0, 4.0],
+            z = [1.0, 2.0, 3.0, 4.0],
+            velocity = [-2.0, -1.0, 1.0, 2.0]
+        )
+
+        hod_params = [10^13, 0.1, 10^14-1.0, 1.0, 1.0, 2.0, 0.0, true, -1000.0, 1000.0]
+
+        x_out = [3.0, 4.0, 3.0, 4.0]
+        y_out = [3.0, 4.0, 3.0, 4.0]
+        z_out = [0.5, 999.5, 2.0, −991.5]
+        count_out = 4
+        @mock rand() = 0.5 begin
+            @mock randn() = 0.5 begin
+                x, y, z, count = populate_galaxies(halos, subhalos, hod_params)
+            end
+        end
+
+        @test isapprox(x, x_out, atol = 1e-4)
+        @test isapprox(y, y_out)
+        @test isapprox(z, z_out)
+        @test isapprox(count, count_out)
+    end
+
 end
