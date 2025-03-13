a4d2d23b114056ae4ae9480f04ba5b30ba9bde4c
Lado Samushia <lado.samushia.office@gmail.com>
2025-03-13 12:15:03 -0500

    print out arrays for debug fix

--- python/tests/test_hod.py
+++ python/tests/test_hod.py
@@ -179,7 +179,7 @@ class TestHODWrapper:
 
         # Since there could be randomness in the HOD, sort arrays before comparing
         # to account for potential different ordering
-        print(np.sort(py_results["x"]))
+        print(np.sort(py_result["x"]))
         print(np.sort(julia_x))
         assert np.allclose(np.sort(py_result["x"]), np.sort(julia_x)), "X coordinates don't match"
         assert np.allclose(np.sort(py_result["y"]), np.sort(julia_y)), "Y coordinates don't match"
