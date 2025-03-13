d9873a06f5e49359383379137543ba5e479f8b02
Lado Samushia <lado.samushia.office@gmail.com>
2025-03-13 12:33:33 -0500

    make M1 really low in the test

--- python/tests/test_hod.py
+++ python/tests/test_hod.py
@@ -70,7 +70,7 @@ class TestHODWrapper:
         hod_params = {
             "lnMcut": 8.0,
             "sigma": 0.2,
-            "lnM1": 11.0,
+            "lnM1": 8.0,
             "kappa": 1.0,
             "alpha": 1.0,
             "alpha_c": 0.0,
